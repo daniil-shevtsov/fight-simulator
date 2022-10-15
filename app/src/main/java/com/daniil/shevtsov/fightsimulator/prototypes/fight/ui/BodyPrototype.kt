cf09eced438ecd0e808ccb066b4a3d0f69968db9
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-10-15 10:18:15 +0300

    Something does not work

--- app/src/main/java/com/daniil/shevtsov/fightsimulator/prototypes/fight/ui/BodyPrototype.kt
+++ app/src/main/java/com/daniil/shevtsov/fightsimulator/prototypes/fight/ui/BodyPrototype.kt
@@ -1,7 +1,6 @@
 package com.daniil.shevtsov.fightsimulator.prototypes.fight.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
@@ -54,125 +53,6 @@ fun CustomLayoutPreview() {
         }
     }
 }
-//@Preview(heightDp = 800, widthDp = 200)
-//@Composable
-//fun ConnectedBodyPrototype() {
-//    Column(
-//        horizontalAlignment = Alignment.CenterHorizontally,
-//    ) {
-//        val headSize = Size(width = 50.dp, height = 50.dp)
-//        val upperArmSize = Size(width = 25.dp, height = headSize.height * 1.5f)
-//        val lowerArmSize = Size(width = 25.dp, height = headSize.height)
-//        val handSize = Size(width = headSize.width / 2f, height = headSize.height * 0.5f)
-//        val upperBodySize = Size(width = headSize.width * 2f, height = headSize.height)
-//        val lowerBodySize = Size(width = headSize.width * 1.5f, height = headSize.height)
-//        val groinSize = Size(width = headSize.width, height = headSize.height)
-//        val upperLegSize = Size(width = 25.dp, height = headSize.height * 1.5f)
-//        val lowerLegSize = Size(width = 25.dp, height = headSize.height)
-//        val feetSize = Size(width = 50.dp, height = 25.dp)
-//        PrototypeBodyPart(
-//            name = "Head",
-//            modifier = Modifier
-//                .size(height = headSize.height, width = headSize.width)
-//        )
-//        PrototypeJoint(orientation = JointOrientation.Horizontal)
-//        Row() {
-//            Column {
-//                PrototypeBodyPart(
-//                    name = "Right Upper Arm",
-//                    modifier = Modifier.size(upperArmSize)
-//                )
-//                PrototypeJoint(orientation = JointOrientation.Horizontal)
-//                PrototypeBodyPart(
-//                    name = "Right Lower Arm",
-//                    modifier = Modifier.size(lowerArmSize)
-//                )
-//                PrototypeJoint(orientation = JointOrientation.Horizontal)
-//                PrototypeBodyPart(
-//                    name = "Left Hand",
-//                    modifier = Modifier.size(handSize)
-//                )
-//            }
-//            PrototypeJoint(orientation = JointOrientation.Vertical)
-//
-//            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-//                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-//                    PrototypeBodyPart(
-//                        name = "Upper Body",
-//                        modifier = Modifier.size(upperBodySize)
-//                    )
-//                    PrototypeJoint(orientation = JointOrientation.Horizontal)
-//                    PrototypeBodyPart(
-//                        name = "Lower Body",
-//                        modifier = Modifier.size(lowerBodySize)
-//                    )
-//                }
-//                PrototypeJoint(orientation = JointOrientation.Horizontal)
-//                Row {
-////                    Column {
-////                        PrototypeBodyPart(
-////                            name = "Right Upper Leg",
-////                            modifier = Modifier.size(upperLegSize)
-////                        )
-////                        PrototypeJoint(orientation = JointOrientation.Horizontal)
-////                        PrototypeBodyPart(
-////                            name = "Right Lower Leg",
-////                            modifier = Modifier.size(lowerLegSize)
-////                        )
-////                        PrototypeJoint(orientation = JointOrientation.Horizontal)
-////                        PrototypeBodyPart(
-////                            name = "Right Foot",
-////                            modifier = Modifier.size(feetSize)
-////                        )
-////                    }
-////                    PrototypeJoint(orientation = JointOrientation.Vertical)
-//                    PrototypeBodyPart(
-//                        name = "Groin",
-//                        modifier = Modifier.size(groinSize)
-//                    )
-////                    PrototypeJoint(orientation = JointOrientation.Vertical)
-////                    Column {
-////                        PrototypeBodyPart(
-////                            name = "Left Upper Leg",
-////                            modifier = Modifier.size(upperLegSize)
-////                        )
-////                        PrototypeJoint(orientation = JointOrientation.Horizontal)
-////                        PrototypeBodyPart(
-////                            name = "Left Lower Leg",
-////                            modifier = Modifier.size(lowerLegSize)
-////                        )
-////                        PrototypeJoint(orientation = JointOrientation.Horizontal)
-////                        PrototypeBodyPart(
-////                            name = "Left Foot",
-////                            modifier = Modifier.size(feetSize)
-////                        )
-////                    }
-//                }
-//
-//            }
-//
-//            PrototypeJoint(orientation = JointOrientation.Vertical)
-//            Column {
-//                PrototypeBodyPart(
-//                    name = "Left Upper Arm",
-//                    modifier = Modifier.size(upperArmSize)
-//                )
-//                PrototypeJoint(orientation = JointOrientation.Horizontal)
-//                PrototypeBodyPart(
-//                    name = "Left Lower Arm",
-//                    modifier = Modifier.size(lowerArmSize)
-//                )
-//                PrototypeJoint(orientation = JointOrientation.Horizontal)
-//                PrototypeBodyPart(
-//                    name = "Left Hand",
-//                    modifier = Modifier.size(handSize)
-//                )
-//            }
-//
-//        }
-//    }
-//}
-
 
 data class BodyPart(
     val id: Long,
@@ -189,12 +69,6 @@ enum class BodyPartType {
     Leg
 }
 
-data class Joint(
-    val id: Long,
-    val start: Long,
-    val end: Long,
-)
-
 @Composable
 fun PrototypeSimpleBodyPart(
     part: BodyPart,
@@ -210,62 +84,81 @@ fun PrototypeSimpleBodyPart(
 }
 
 @Composable
-private fun PrototypeJoint(
-    modifier: Modifier = Modifier,
-    orientation: JointOrientation = JointOrientation.Horizontal,
-) {
-    val jointWidth: Dp
-    val jointHeight: Dp
-
-    when (orientation) {
-        JointOrientation.Horizontal -> {
-            jointWidth = 25.dp
-            jointHeight = 10.dp
-        }
-        JointOrientation.Vertical -> {
-            jointWidth = 10.dp
-            jointHeight = 25.dp
-        }
-    }
-
-    Box(
-        modifier = modifier
-            .background(Color.Cyan)
-            .size(width = jointWidth, height = jointHeight)
-    ) {
-
-    }
-}
-
-@Composable
 fun CustomBodyLayout(
     prototypeBody: List<BodyPart>,
     content: @Composable () -> Unit
 ) = Layout(content) { measurables, constraints ->
-    val measuredPlaceables = measurables.map { measurable ->
-        measurable.measure(constraints) to measurable.layoutId as BodyPart
+    val unmeasured = measurables.map { measurable ->
+        measurable to measurable.layoutId as BodyPart
+    }.toMap()
+    val numberOfArmRows =
+        unmeasured.count { (_, bodyPart) -> bodyPart.type == BodyPartType.Arm } / 2
+
+    val measuredPlaceables = unmeasured.map { (measurable, bodyPart) ->
+        val measured = when (bodyPart.type) {
+            BodyPartType.Body -> {
+                measurable.measure(
+                    constraints.copy(
+                        minHeight = constraints.minHeight * numberOfArmRows,
+                        maxHeight = constraints.maxHeight * numberOfArmRows,
+                    )
+                )
+            }
+            else -> {
+                measurable.measure(constraints)
+            }
+        }
+
+        bodyPart to measured
     }.toMap()
 
     // 2. The sizing phase.
     layout(constraints.maxWidth, constraints.maxHeight) {
         // 3. The placement phase.
         var lastBodyPosition = Offset(0f, 0f)
-        val placeablesWithPositions = measuredPlaceables.map { (placeable, bodyPart) ->
-            val position = when (bodyPart.type) {
-                BodyPartType.Head -> Offset(
-                    x = constraints.maxWidth.toFloat() / 2 - placeable.width.toFloat() / 2,
-                    y = 0f
-                )
-                else -> {
-                    Offset(
+        val nonLimbPlaceablesWithPositions = measuredPlaceables
+            .filter { (bodyPart, _) -> bodyPart.type != BodyPartType.Arm && bodyPart.type != BodyPartType.Leg }
+            .map { (bodyPart, placeable) ->
+                val position = when (bodyPart.type) {
+                    BodyPartType.Head -> Offset(
                         x = constraints.maxWidth.toFloat() / 2 - placeable.width.toFloat() / 2,
-                        y = lastBodyPosition.y + placeable.height
+                        y = 0f
                     )
+                    else -> {
+                        Offset(
+                            x = constraints.maxWidth.toFloat() / 2 - placeable.width.toFloat() / 2,
+                            y = lastBodyPosition.y + placeable.height
+                        )
+                    }
                 }
+                placeable to position
             }
-            lastBodyPosition = position
-            placeable to position
-        }
+
+        val limbPlaceablesWithPositions = measuredPlaceables
+            .filter { (bodyPart, _) -> bodyPart.type == BodyPartType.Arm && bodyPart.type == BodyPartType.Leg }
+            .map { (bodyPart, placeable) ->
+                val position = when (bodyPart.type) {
+                    else -> {
+                        val bodyPosition =
+                            nonLimbPlaceablesWithPositions.find { (placeable, position) ->
+                                val (bodyPart, bodyPlaceable) = measuredPlaceables.entries.find { (bodyPart, placeable) -> bodyPart.type == BodyPartType.Body }
+                                    ?.toPair()!!
+                                placeable == bodyPlaceable
+                            }?.second
+                        if (bodyPosition != null) {
+                            Offset(
+                                x = bodyPosition.x - placeable.width,
+                                y = bodyPosition.y
+                            )
+                        } else {
+                            Offset(0f, 0f)
+                        }
+                    }
+                }
+                placeable to position
+            }
+
+        val placeablesWithPositions = nonLimbPlaceablesWithPositions + limbPlaceablesWithPositions
 
         placeablesWithPositions.forEach { (placeable, position) ->
             placeable.place(x = position.x.toInt(), y = position.y.toInt())
